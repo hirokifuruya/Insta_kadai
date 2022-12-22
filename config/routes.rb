ffd798aa32647765453e350c5f3c76079ab23b96
古谷啓起 <furuyahiroki@furuyakeikinoMacBook-Air.local>
2022-12-22 09:56:36 +0900

    エラー画面編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'blogs#index'
   resources :favorites, only: [:create, :destroy]
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
